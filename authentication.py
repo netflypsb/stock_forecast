e021ca164b71fe8d3da3d21e260c2390ea6604a7
netflypsb <154873617+netflypsb@users.noreply.github.com>
2024-04-24 14:27:45 +0800

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -8,11 +8,18 @@ def fetch_subscribers(access_token=None):
     url = "https://developers.buymeacoffee.com/api/v1/subscriptions"
     headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()['data']
-    else:
-        raise Exception("Failed to fetch subscribers: {} - {}".format(response.status_code, response.text))
 
-def is_subscriber_authorized(email, required_plan, subscribers):
-    """Check if the provided email is subscribed to the required plan."""
-    return any(sub['payer_email'] == email and sub['plan'] == required_plan for sub in subscribers)
+    # Check if the response was successful
+    if response.status_code == 200:
+        json_response = response.json()
+        # Check if 'data' key is present in the response
+        if 'data' in json_response:
+            return json_response['data']
+        else:
+            # Log error or handle cases where 'data' is not present
+            st.error("Received unexpected data structure from BMAC API.")
+            return []
+    else:
+        # Handle response errors
+        st.error(f"Failed to fetch subscribers: {response.status_code} - {response.text}")
+        return []
